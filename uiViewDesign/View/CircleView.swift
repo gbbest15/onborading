666df158264f83c0d754e2fd13d5d8a908900f26
Ayannuga Gbenga <65336545+gbbest15@users.noreply.github.com>
2023-01-10 09:20:44 +0100

    update on homeScreen

---
+++ uiViewDesign/View/CircleView.swift
@@ -0,0 +1,20 @@
+//
+//  CircleView.swift
+//  uiViewDesign
+//
+//  Created by G BEN on 10/01/2023.
+//
+
+import SwiftUI
+
+struct CircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleView()
+    }
+}
