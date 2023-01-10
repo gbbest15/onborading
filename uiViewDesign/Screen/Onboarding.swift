58e957b46ef8e12c66bc9a4af4974bb7a960db42
Ayannuga Gbenga <65336545+gbbest15@users.noreply.github.com>
2023-01-10 13:41:58 +0100

    responses to user when draging button on unboard

--- uiViewDesign/Screen/Onboarding.swift
+++ uiViewDesign/Screen/Onboarding.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 
 struct Onboarding: View {
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
     @AppStorage("onboard")  var userOnboardingValue : Bool = true
-
+    @State var offsetButton:CGFloat = 0
+    
     var body: some View {
         ZStack {
             Color("ColorBlue")
@@ -37,45 +39,61 @@ how much love we put into it
                 }
                 Spacer()
                 ZStack {
-                  // PARTS OF THE CUSTOM BUTTON
-                  Capsule()
-                    .fill(Color.white.opacity(0.2))
-                  Capsule()
-                    .fill(Color.white.opacity(0.2))
-                    .padding(8)
-                  // 2. CALL-TO-ACTION (STATIC)
-                  Text("Get Started")
-                    .font(.system(.title3, design: .rounded))
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                    .offset(x: 20)
-                  // 3. CAPSULE (DYNAMIC WIDTH)
-                  HStack {
+                    // PARTS OF THE CUSTOM BUTTON
                     Capsule()
-                      .fill(Color("ColorRed"))
-                      .frame(width:  80)
-                    Spacer()
-                  }
-                  // 4. CIRCLE (DRAGGABLE)
-                  HStack {
-                    ZStack {
-                      Circle()
-                        .fill(Color("ColorRed"))
-                      Circle()
-                        .fill(.black.opacity(0.15))
+                        .fill(Color.white.opacity(0.2))
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
                         .padding(8)
-                      Image(systemName: "chevron.right.2")
-                        .font(.system(size: 24, weight: .bold))
+                    // 2. CALL-TO-ACTION (STATIC)
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    // 3. CAPSULE (DYNAMIC WIDTH)
+                    
+                    
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width:  80)
+                        Spacer()
                     }
-                    .foregroundColor(.white)
-                    .frame(width: 80, height: 80, alignment: .center)
-                     //: GESTURE
-                    Spacer()
-                  } //: HSTACK
+                    // 4. CIRCLE (DRAGGABLE)
+                    HStack {
+                        
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .offset(x: offsetButton)
+                        .gesture(DragGesture().onChanged({ v in
+                            if v.translation.width > 0 && offsetButton <= buttonWidth - 80 {
+                                offsetButton = v.translation.width
+                            }
+                        }).onEnded({ value in
+                            if self.offsetButton > buttonWidth / 2 {
+                                self.offsetButton = buttonWidth - 80
+                                self.userOnboardingValue = false
+                            } else {
+                                self.offsetButton = 0
+                            }
+                        }))
+                        //: GESTURE
+                        Spacer()
+                    } //: HSTACK
                 } //: FOOTER
-                .frame( height: 80, alignment: .center)
+                .frame(width: buttonWidth, height: 80, alignment: .center)
                 .padding()
-            
+                
             }
             
         }
